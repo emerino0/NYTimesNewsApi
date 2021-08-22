73012f1dee3cff6c09e3c0b9106dadf08b0a9890
Kike <e.merinog.job@gmail.com>
2021-08-22 22:37:17 +0200

    ViewBinding in views. v1.2

--- app/src/main/java/com/example/nytimesNewsApi/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/nytimesNewsApi/presentation/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.nytimesNewsApi.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.nytimesNewsApi.R
+import com.example.nytimesNewsApi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
